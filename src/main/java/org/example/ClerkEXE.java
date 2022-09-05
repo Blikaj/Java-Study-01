91aeb23cd834aed8abe54a3ffa50858f6397c55a
Blikaj <alex.vs1910@gmail.com>
2022-09-05 23:31:50 +0300

    V 0.1 | 05-09 Дз задание 1 сделано

--- src/main/java/org/example/ClerkEXE.java
+++ src/main/java/org/example/ClerkEXE.java
@@ -2,9 +2,12 @@ package org.example;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
 
 public class ClerkEXE {
 
@@ -12,8 +15,21 @@ public class ClerkEXE {
         ArrayList<Clerk> clerkArrayList;
         Scanner in = new Scanner(System.in);
         clerkArrayList = new ArrayList<>();
-        int ch = -1;
-        while (ch != 6) {
+        ArrayList<String> tmpclerk = new ArrayList<String>();
+        String ch = "-1";
+        try (FileReader reader = new FileReader("C:\\Users\\Bocus\\IdeaProjects\\Java-Study-01\\src\\main\\java\\org\\example\\clerks.txt")) {
+            Scanner scan = new Scanner(reader);
+            while (scan.hasNext()){
+                tmpclerk.add(scan.nextLine());
+            }
+            reader.close();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        while (ch != "6") {
             System.out.print(
                     """
                             ============Text Main Menu =============
@@ -25,12 +41,59 @@ public class ClerkEXE {
                             5- print salary sum
                             6- exit
                             """);
-            ch = in.nextInt();
+            ch = in.next();
             switch (ch) {
-                case 1: {
-                    try (FileReader reader = new FileReader("C:\\Users\\alexv\\IdeaProjects\\MyFirstProject\\src\\main\\java\\org\\example\\clerks.txt")) {
-                        while (reader.ready()){
-                            Clerk tmpclerk = reader.readline();
+
+                case "1": {
+                    tmpclerk.forEach(clerkk ->{
+                        System.out.println(clerkk);
+                    });
+                    break;
+                }
+                case "2": {
+                    ch="2";
+                    try (FileWriter writer = new FileWriter("C:\\Users\\Bocus\\IdeaProjects\\Java-Study-01\\src\\main\\java\\org\\example\\clerks.txt")) {
+                        System.out.println("""
+                                Hello, this is the clerk adding algorithm.
+                                To add clerk type "add".
+                                
+                                If you are done with adding new clerks, type 'done' """);
+                        String inchar = in.next();
+                        switch (inchar) {
+                            case "add": {
+                                System.out.println("""
+                                        Type clerk info as follows:
+                                        ID(String);Name(String);Birthday(Date);Salary(Double)""");
+                                String tmpdat = in.next()+"\n";
+                                int c = 0;
+                                for (String clerkk : tmpclerk) {
+                                    if (clerkk.contains(tmpdat.split(";")[0])) {
+                                        System.out.print("Err, ID already exists!");
+                                        c++;
+                                    }
+                                }
+                                if (c == 0) {
+                                    tmpclerk.add(tmpdat);
+                                    tmpclerk.forEach(clerk ->{
+                                        try {
+                                            writer.write(String.valueOf(clerk+"\n"));
+                                        } catch (IOException e) {
+                                            throw new RuntimeException(e);
+                                        }
+                                    });
+                                    System.out.print("Clerk added");
+                                    }
+                                writer.close();
+                                break;
+                            }
+                            case "done": {
+                                writer.close();
+                                break;
+                            }
+                            default: {
+                                System.out.print("Wrong choise, my friend!");
+                                break;
+                            }
                         }
 
                     } catch (FileNotFoundException e) {
@@ -38,14 +101,63 @@ public class ClerkEXE {
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
+                    break;
+                }
+                case "3": {
+                    System.out.println("Type ID, name or birthday date of an employee:");
+                    ArrayList<String> outList = new ArrayList<String>();
+                    String inchar = in.next();
+                    tmpclerk.forEach(clerkk ->{
+                        ///System.out.println("dot");
+                        if (clerkk.contains(inchar)){
+                            outList.add(clerkk);
+                        }
+                    });
+                    System.out.println(outList);
+                    break;
+                }
+                case "4":{
+                    System.out.println("Type ID of the employee:");
+                    String inchar = in.next();
+                    for (String clerkk : tmpclerk){
+                        if (clerkk.contains(inchar)){
+                            System.out.println("Type new info: Name;Birthday;Salary");
+                            String alter = in.next();
+                            tmpclerk.set(tmpclerk.indexOf(clerkk), inchar+";"+alter);
+                        }
+                    }
+                    try (FileWriter writer = new FileWriter("C:\\Users\\Bocus\\IdeaProjects\\Java-Study-01\\src\\main\\java\\org\\example\\clerks.txt")) {
+                        tmpclerk.forEach(clerkk ->{
+                            try {
+                                writer.write(String.valueOf(clerkk+"\n"));
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+                        System.out.print("Clerk info altered!");
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    break;
+                }
+                case "5": {
+                    System.out.println("The sum of the salaries is:");
+                    Double outSal = Double.valueOf(0);
+                    for (String clerkk : tmpclerk){
+                        outSal += Double.parseDouble(clerkk.split(";")[3]);
+                    }
+                    System.out.println(outSal);
+                    break;
                 }
 
-                case 6: {
+                case "6": {
+                    ch = "6";
                     System.out.print("Exiting...");
                     break;
                 }
                 default: {
-                    ch = -1;
+                    ch = "-1";
                     System.out.print("Wrong choise, my friend!");
                     break;
                 }
@@ -53,6 +165,5 @@ public class ClerkEXE {
         }
 
     }
-
 
     }
